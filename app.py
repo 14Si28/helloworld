b289e26841b2dd12f2c58102d41838b787e05e7b
14Si28 <aranoopsafe987@gmail.com>
2018-04-19 15:37:03 +0530

    swap app

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 
 print('hello world')
+
+def swap(num1,num2):
+    num1=num1+num2
+    num2=num1-num2
+    num1=num1-num2
+    return num1,num2
+
+x=input("Enter a number: ")
+y=input("Enter a number: ")
+print "before swap", (x,y)
+print "after swap", swap(x,y)
