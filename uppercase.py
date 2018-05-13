12092b2a0e7c99859a6510fedda915bfccb77183
14Si28 <aranoopsafe987@gmail.com>
2018-05-14 02:05:22 +0530

    uppercase

---
+++ uppercase.py
@@ -0,0 +1,10 @@
+class InputOutString():
+     def getString(sk):
+         sk.s = input()
+
+     def printString(sk1):
+        print (sk1.s.upper())
+
+strObj = InputOutString()
+strObj.getString()
+strObj.printString()
